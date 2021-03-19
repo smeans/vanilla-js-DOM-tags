343513bf4278167454c917d8b4f8f3f1c014163c
Scott Means <smeans@gmail.com>
2021-03-19 10:53:44 +0700

    use Array.prototype to access forEach

--- Chapter1/01_02/cookies.js
+++ Chapter1/01_02/cookies.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ingredients = document.getElementById('ingredients');
 
-    ingredients.children.forEach((tr) => {
+    Array.prototype.forEach.call(ingredients.children, (tr) => {
         console.log(tr);
     });
+
+    for (let i = 0; i < ingredients.children.length; i++) {
+        console.log(ingredients.children[i]);
+    }
 });
