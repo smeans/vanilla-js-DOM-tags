64723e2e2bd599f7375a34202631280b4637cf24
Scott Means <smeans@gmail.com>
2021-03-19 10:54:57 +0700

    add code to total the ingredient weights

--- Chapter1/01_02/cookies.js
+++ Chapter1/01_02/cookies.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ingredients = document.getElementById('ingredients');
 
+    let total = 0;
+
     Array.prototype.forEach.call(ingredients.children, (tr) => {
         console.log(tr);
+        let td = tr.children[1];
+        let weight = td.nodeValue;
+        console.log(td, weight);
     });
-
-    for (let i = 0; i < ingredients.children.length; i++) {
-        console.log(ingredients.children[i]);
-    }
 });
